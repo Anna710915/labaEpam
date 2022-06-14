f9fe6a6428b12895997a6a23e3f144e513a99963
Anna Merkul <anna.merkul@bk.ru>
2022-06-14 19:02:46 +0300

    SpringBootServletInitializer

--- controller/src/main/java/com/epam/esm/config/ControllerConfig.java
+++ controller/src/main/java/com/epam/esm/config/ControllerConfig.java
@@ -2,6 +2,8 @@ package com.epam.esm.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.PropertySource;
 
 /**
@@ -10,7 +12,12 @@ import org.springframework.context.annotation.PropertySource;
  */
 @SpringBootApplication(scanBasePackages = "com.epam.esm")
 @PropertySource({"classpath:token.properties"})
-public class ControllerConfig {
+public class ControllerConfig extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ControllerConfig.class);
+    }
     /**
      * The entry point of application.
      *
